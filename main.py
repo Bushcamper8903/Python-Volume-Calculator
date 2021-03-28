490671ce76fbb5437e7b8547a7ee550e5e3c3bcc
Bushcamper8903 <advayvchitnis@gmail.com>
2021-03-28 15:20:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Python class
+class calculation:
+    #Shape measurements
+    height = int(input('Height: '))
+    width = int(input('Width: '))
+    depth = int(input('Depth: '))
+
+    #Unit options
+    units = ['mm', 'cm', 'm']
+    print(units)
+
+    unit = str(input('Unit: '))
+
+    #Volume formula
+    volume = height * width * depth
+
+    #Print answer
+    if unit in units:
+        print(f'Volume: {volume}{unit}³')
+
+    #Error checks
+    else:
+        print('Error when calculating unit')
